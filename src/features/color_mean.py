21e8ad579bf183ab77f3db673a5e65a8af9514da
Jules <pikachupigmente@gmail.com>
2021-04-01 11:05:41 +0200

    Color mean unit test, debug and coms

--- src/features/color_mean.py
+++ src/features/color_mean.py
@@ -2,13 +2,11 @@ import cv2
 import numpy as np
 
 from features import feature_diff
+from utils import get_points, get_axes, make_quadrants
 
 
 # In : Quadrants, image
 # Out : Mean color for each quadrant
-from utils import get_points, get_axes, make_quadrants
-
-
 def get_mean_colors(quadrants, img):
     moyennes = []
     for quad_num in range(4):
@@ -28,7 +26,7 @@ def f_color_mean(img, quadrants):
 # In : Quadrants, image, moyennes des couleurs par quadrant
 # Out : Image with mean color by quadrant
 def draw_mean_colors(quadrants, img, moyennes):
-    result = np.zeros(img.shape)
+    result = np.zeros(img.shape, dtype=np.uint8)
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
             if quadrants[i][j] != -1:
@@ -40,13 +38,29 @@ def draw_mean_colors(quadrants, img, moyennes):
 
 if __name__ == "__main__":
     IMG_SIZE = (300,300)
-    path = '/content/ISIC_2019/SEGMENTEES/HIGH_RESOL_FILLED/TEST/MEL/ISIC_0033710.JPG'
-    mask_path = '/content/ISIC_2019/SEGMENTEES/MASK/TEST/MEL/ISIC_0033710_Mask.jpg'
+    win_name = 'Coucou toi, je suis un melanome'
+    path = '../../images/melanoma.jpg'
+    mask_path = '../../images/melanomask.jpg'
     img = cv2.resize(cv2.imread(path), IMG_SIZE)
     mask = cv2.resize(cv2.imread(mask_path, 0), IMG_SIZE)
     points = np.asarray(get_points(mask))
     axe1, axe2 = get_axes(points)
     quadrants = make_quadrants(mask, points, axe1, axe2)
     moyennes = get_mean_colors(quadrants, img)
-    cv2.imshow(img)
+    cv2.imshow(win_name, img)
+    cv2.waitKey(0)
+    res = draw_mean_colors(quadrants, img, moyennes)
+    cv2.imshow(win_name, res)
+    cv2.waitKey(0)
+    moyennesV = [(moyennes[0] + moyennes[1])/2, (moyennes[2] + moyennes[3])/2]
+    moyennesV = [moyennesV[0], moyennesV[0], moyennesV[1], moyennesV[1]]
+    res = draw_mean_colors(quadrants, img, moyennesV)
+    cv2.imshow(win_name, res)
+    cv2.waitKey(0)
+    moyennesH = [(moyennes[0] + moyennes[2])/2, (moyennes[1] + moyennes[3])/2]
+    moyennesH = [moyennesH[0], moyennesH[1], moyennesH[0], moyennesH[1]]
+    res = draw_mean_colors(quadrants, img, moyennesH)
+    cv2.imshow(win_name, res)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
     print("Everything passed")
--- src/features/utils.py
+++ src/features/utils.py
@@ -1,21 +1,23 @@
-import math
-
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
-from numpy import round
-from numpy.linalg import det
+from numpy import cos, sin
 from sklearn.decomposition import PCA
 
 
+# In : Grayscale image
+# Out : Number of black pixels
 def get_area(img):
     return len([pixel for row in img for pixel in row if pixel == 0])
 
 
+# In : Grayscale image
+# Out : Numpy array of points representing white pixels of the image
 def get_points(img):
     return np.asarray([(j, i) for i, row in enumerate(img) for j, pixel in enumerate(row) if pixel != 0])
 
 
+# In : Array of points
+# Out : Coordinates of the mean coordinates
 def get_center(points):
     length = points.shape[0]
     sum_x = np.sum(points[:, 0])
@@ -23,6 +25,8 @@ def get_center(points):
     return sum_x / length, sum_y / length
 
 
+# In : Grayscale image (mask of the lesion)
+# Out : Angle of rotation minimizing the area difference between the two halves of image
 def get_axis_area(mask):
     points = np.asarray(get_points(mask))
     center = get_center(points)
@@ -41,108 +45,77 @@ def get_axis_area(mask):
     return np.argmin([aires[i] for i in aires.keys()]) * 10
 
 
+# In : Point p, line d
+# Out : True if determinant is negative, False otherwise
 def relative_side_of_point(p, d):
     return (d[0][0] - p[0]) * (d[1][1] - p[1]) - (d[0][1] - p[1]) * (d[1][0] - p[0]) < 0
 
 
-def get_axes(points):
+# In : List of points, method used to compute the symmetry axes
+# Out : Symmetry axes
+# method possible values : PCA, Minim
+# PCA : Compute axes using PCA
+# Minim : Compute axes using area difference minimization
+def get_axes(points, method='PCA'):
     cX, cY = get_center(points)
-    pca = PCA(n_components=2).fit(points)
-    axes = []
-    for i, (comp, var) in enumerate(zip(pca.components_, pca.explained_variance_ratio_)):
-        comp = comp * var * 400
-        axes.append([[cX, cY], [cX + comp[0], cY + comp[1]]])
-    return np.asarray(axes)
+    if method == 'PCA':
+        pca = PCA(n_components=2).fit(points)
+        axes = []
+        for i, (comp, var) in enumerate(zip(pca.components_, pca.explained_variance_ratio_)):
+            comp = comp * var * 400
+            axes.append([[cX, cY], [cX + comp[0], cY + comp[1]]])
+        return axes
+    elif method == 'Minim':
+        best_angle = get_axis_area(mask)
+
+        enX = int(400 * cos(-best_angle))
+        enY = int(400 * sin(-best_angle))
+
+        axe1 = ((cX - enX, cY - enY), (cX + enX, cY + enY))
+        axe2 = ((cX + enY, cY - enX), (cX - enY, cY + enX))
+        return axe1, axe2
+    return None
 
 
-def make_quadrants(points, axe1, axe2):
-    quadrants = []
+# In : Mask of the lesion, Points, axe1, axe2
+# Out : Quadrants of the image (mask.shape)
+# Quadrants has 4 possible values : 
+# -1 : Not in the lesion, 0,1,2,3 : Quad number of the (i,j) pixel
+def make_quadrants(mask, points, axe1, axe2):
+    quadrants = np.ones((mask.shape[0], mask.shape[1]), dtype=np.uint8) * -1
     for p in points:
+        i = p[1]
+        j = p[0]
         if relative_side_of_point(p, axe1):
             if relative_side_of_point(p, axe2):
-                quadrants.append(0)
+                quadrants[i][j] = 0
             else:
-                quadrants.append(1)
+                quadrants[i][j] = 1
         else:
             if relative_side_of_point(p, axe2):
-                quadrants.append(2)
+                quadrants[i][j] = 2
             else:
-                quadrants.append(3)
-    return np.asarray(quadrants)
+                quadrants[i][j] = 3
+    return quadrants
 
 
-def quad_on_img(img, img_points, alpha):
-    res = np.zeros(img.shape, dtype=np.uint8)
+# In : Image, quadrants, alpha
+# Out : Resulting image of the fusion between alpha*img + (1-alpha)*quadrants
+# alpha is the percentage of the original image
+def quad_on_img(img, quadrants, alpha):
+    img_points = np.zeros(img.shape, dtype=np.uint8)
+    for i, row in enumerate(img):
+        for j, pixel in enumerate(row):
+            img_points[i][j] = np.asarray(quad_rgb[quadrants[i][j]])
+
+    res = np.zeros((img.shape), dtype=np.uint8)
     for i, row in enumerate(img_points):
         for j, pixel in enumerate(row):
-            if pixel.any() == 0:
+            if pixel.any() == -1:
                 res[i][j] = img[i][j]
             else:
                 res[i][j] = img[i][j] * alpha + img_points[i][j] * (1 - alpha)
     return res
-
-
-def points2img(points, img, quadrants):
-    img_points = np.zeros(img.shape, dtype=np.uint8)
-    for num, point in enumerate(points):
-        i = point[0]
-        j = point[1]
-        img_points[j][i] = np.asarray(quad_rgb[quadrants[num]])
-    return img_points
-
-
-def plot_points(x, y, alpha=1, c=None, s=1):
-    plt.scatter(x, y, alpha=alpha, c=c, s=s)
-    ax = plt.gca()
-    ax.set_ylim(ax.get_ylim()[::-1])
-    plt.gca().set_aspect('equal', adjustable='box')
-
-
-def center_img(gray):
-    height, width = gray.shape
-    wi = width / 2
-    he = height / 2
-    ret, thres = cv2.threshold(gray, 95, 255, 0)
-    M = cv2.moments(thres)
-    cX = int(M["m10"] / M["m00"])
-    cY = int(M["m01"] / M["m00"])
-    cv2.circle(gray, (round(cX), round(cY)), 2, (0, 255, 0), -1)
-    offsetX = wi - cX
-    offsetY = he - cY
-    T = np.float32([[1, 0, offsetX], [0, 1, offsetY]])
-    centered_img = cv2.warpAffine(gray, T, (width, height))
-    return centered_img
-
-
-def crop_lesion(mask, img):
-    points = cv2.findNonZero(mask)
-    x, y, w, h = cv2.boundingRect(points)
-    return img[y:y + h, x:x + w]
-
-
-def get_full_quads(quads, points, img):
-    full_quads = np.ones((img.shape[0], img.shape[1])) * -1
-    for i, (x, y) in enumerate(points):
-        full_quads[y][x] = quads[i]
-    return full_quads
-
-
-def draw_mean_colors(full_quads, img, moyennes):
-    result = np.zeros(img.shape)
-    for i in range(img.shape[0]):
-        for j in range(img.shape[1]):
-            if full_quads[i][j] != -1:
-                result[i][j] = img[i][j] * 0 + moyennes[int(full_quads[i][j])] * 1
-            else:
-                result[i][j] = img[i][j]
-    return result
-
-
-def get_angle(vector_1, vector_2):
-    unit_vector_1 = vector_1 / np.linalg.norm(vector_1)
-    unit_vector_2 = vector_2 / np.linalg.norm(vector_2)
-    dot_product = np.dot(unit_vector_1, unit_vector_2)
-    return math.degrees(np.arccos(dot_product))
 
 
 def rotate_img(image, angle, center, scale=1.):
@@ -152,33 +125,5 @@ def rotate_img(image, angle, center, scale=1.):
     return rotated
 
 
-def cut_image(img):
-    results = np.zeros((4, int(img.shape[0] / 2), int(img.shape[1] / 2), img.shape[2]))
-    results[0] = img[:int(img.shape[0] / 2), int(img.shape[1] / 2):]
-    results[1] = img[int(img.shape[0] / 2):, int(img.shape[1] / 2):]
-    results[2] = img[:int(img.shape[0] / 2), :int(img.shape[1] / 2)]
-    results[3] = img[int(img.shape[0] / 2):, :int(img.shape[1] / 2)]
-    return results
-
-
-def get_area_diff(mask):
-    somme = 0
-    for i in range(int(mask.shape[0] / 2)):
-        for j in range(mask.shape[1]):
-            somme += int(mask[i][j] != mask[mask.shape[0] - i - 1][j])
-    return somme
-
-
-def get_cropped(img, mask, points, axe1, axe2):
-    center = get_center(points)
-    vector = (axe1[1][0] - axe1[0][0], axe1[1][1] - axe1[0][1])
-    vector2 = (axe2[1][0] - axe2[0][0], axe2[1][1] - axe2[0][1])
-    angle = get_angle((1, 0), vector)
-    img_rotated = rotate_img(img, -angle, center)
-    mask_rotated = rotate_img(mask, -angle, center)
-    cropped = crop_lesion(mask_rotated, img_rotated)
-    cropped = cv2.resize(cropped, img_shape)
-    return cropped
-
-
-quad_rgb = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0)]
+def distance(p1, p2):
+    return np.sqrt(pow(p1[0] - p2[0], 2) + pow(p1[1] - p2[1], 2))
